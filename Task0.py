302f150b5e7aedef6ccf7dfc6a8c0c18b419517e
Amtoni <dsw52717@student.dsw.edu.pl>
2024-06-30 23:24:24 +0200

    task2

--- Task0.py
+++ Task0.py
@@ -14,3 +14,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+    import json
+
+def wczytaj_json(nazwa_pliku):
+    try:
+        with open(nazwa_pliku, 'r', encoding='utf-8') as plik:
+            dane = json.load(plik)
+        return dane
+    except FileNotFoundError:
+        print(f"Plik '{nazwa_pliku}' nie istnieje.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Błąd dekodowania JSON w pliku '{nazwa_pliku}': {e}")
+        return None
+    
+
+
+dane = wczytaj_json('file.json')
+if dane:
+    print(dane)
